e69014614ffb646b29d291231365b1dd4d1f0a2f
Hammad740 <mohammadhammadansari91@gmail.com>
2024-05-13 11:59:24 +0530

    basics

--- 01_Javascript_Basics/variables.js
+++ 01_Javascript_Basics/variables.js
@@ -33,3 +33,40 @@ console.log(myName);
 // using const - can't re-assign
 const lastName = 'jordan';
 console.log(lastName);
+
+//// String concatenation-combine string values
+//// `` - backticks (template strings) is easier option
+const website = 'youtube';
+const url = 'https://www.' + website + '.com';
+console.log(url);
+
+// how js is loosely typed language?
+// Since JavaScript is a loosely typed language, you are not required to correctly predict the kind of data that will be kept in a variable. Depending on the information you supply to a variable (such as this ‘ ‘ or " " to signify string values), JavaScript will automatically type it.
+
+//loosely typed= don't declare type
+
+const number = 34;
+const number2 = 2.45;
+const number3 = '2.45';
+console.log(number, number2, number3);
+
+// +=,-=,*=,/=,++,--,%
+// % operator returns the remainder after integer division
+let no = 40;
+no += 5;
+console.log(no);
+no -= 5;
+console.log(no);
+no *= 5;
+console.log(no);
+no++;
+console.log(no);
+no--;
+console.log(no);
+
+const slices = 100 % 6;
+console.log(slices);
+
+// https://www.w3schools.com/js/js_type_conversion.asp
+
+// must read for type coercion in js

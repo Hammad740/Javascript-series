49ea15308e509d1223c1d8e4bba071e6404a778e
Hammad740 <mohammadhammadansari91@gmail.com>
2024-05-13 12:12:44 +0530

    datatypes

---
+++ 01_Javascript_Basics/datatypes.js
@@ -0,0 +1,67 @@
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+// numbers
+let length = 15;
+let weight = 7.5;
+
+console.log(length, weight);
+
+// strings
+let color = 'Yellow';
+let lastName = 'Johnson';
+
+// booleans
+let x = true;
+let y = false;
+
+// object
+const person = { firstName: 'john', lastName: 'Doe' };
+
+// array object
+const cars = ['volvo', 'bmw', 'maruti'];
+
+// numbers
+// With decimals:
+let x1 = 34.0;
+
+// Without decimals:
+let x2 = 34;
+
+let w = 123e5; // 12300000
+let z = 123e-5; // 0.00123
+
+// bigint
+let a = BigInt('123456789012345678901234567890');
+
+console.log(a);
+
+// undefined-In JavaScript, a variable without a value, has the value undefined. The type is also undefined.
+
+let bike;
+// since value is undefined so type is undefined
+
+// note - An empty value has nothing to do with undefined.
+
+// An empty string has both a legal value and a type.
+
+// JavaScript null
+// In JavaScript, null is a special value that represents an empty or unknown value. For example,
+
+// let number = null;
+
+// In JavaScript, undefined and null are treated as false values.

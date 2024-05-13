1f055561e0afe649cee40bee6715be39724240e7
Hammad740 <mohammadhammadansari91@gmail.com>
2024-05-13 11:38:57 +0530

    initial commit

---
+++ 01_Javascript_Basics/variables.js
@@ -0,0 +1,35 @@
+// // https://www.w3schools.com/js/js_variables.asp
+
+//// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+//// https://www.w3schools.com/js/js_let.asp
+
+//// The let keyword was introduced in ES6 (2015)
+
+//// Variables declared with let have Block Scope
+
+//// Variables declared with let must be Declared before use
+
+////  Variables declared with let cannot be Redeclared in the same scope
+
+//// Naming variables in JavaScript
+// Variable names must start with a letter, an underscore ( _ ) or a dollar sign ( $ ).
+// Variable names cannot contain spaces.
+// Variables cannot be the same as reserved keywords such as if or const .
+// By convention, JavaScript variable names are written in camelCase.
+
+var value = 'some value';
+console.log(value);
+
+let myName = 'john';
+console.log(myName);
+
+// using const - can't re-assign
+const lastName = 'jordan';
+console.log(lastName);

023d9aceb636ea5f88fc5ce125b6590bf48254c2
Hammad740 <mohammadhammadansari91@gmail.com>
2024-05-16 12:03:01 +0530

    objects

---
+++ 01_Javascript_Basics/objects.js
@@ -0,0 +1,45 @@
+//* in js objects are kings,if you understand objects you understand the js
+//* in js almost everything is object
+
+// https://www.w3schools.com/js/js_object_definition.asp
+/**
+ Booleans can be objects (if defined with the new keyword)
+Numbers can be objects (if defined with the new keyword)
+Strings can be objects (if defined with the new keyword)
+Dates are always objects
+Maths are always objects
+Regular expressions are always objects
+Arrays are always objects
+Functions are always objects
+Objects are always objects
+ */
+
+/**
+ All JavaScript values, except primitives, are objects.
+
+JavaScript Primitives
+A primitive value is a value that has no properties or methods.
+
+3.14 is a primitive value
+
+A primitive data type is data that has a primitive value.
+
+JavaScript defines 7 types of primitive data types:
+
+Examples
+string
+number
+boolean
+null
+undefined
+symbol
+bigint
+ */
+
+/// js objects are named values
+
+const person = { firstName: 'John', lastName: 'Doe', age: 50, education: true };
+console.log(person);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
